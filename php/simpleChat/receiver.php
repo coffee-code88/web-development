05d80c70ca0b4b5bc31c93e83d40e6044fcef196
coffee-code <coffee.code88@gmail.com>
2016-03-16 21:03:20 +0700

    modified

--- php/simpleChat/receiver.php
+++ php/simpleChat/receiver.php
@@ -2,7 +2,7 @@
 $fileName="sender.php";
 include 'form.inc.php';
 
-if(isset($_POST['submit']) && !empty($_POST['username'])) {
+if(isset($_POST['submit']) && !empty(trim($_POST['username']))) {
 	echo "From " . $_POST['username'] . ": " . $_POST['msg'];
 }
 
--- php/simpleChat/sender.php
+++ php/simpleChat/sender.php
@@ -3,7 +3,7 @@ $fileName="receiver.php";
 
 include 'form.inc.php';
 
-if(isset($_POST['submit']) && !empty($_POST['username'])) {
+if(isset($_POST['submit']) && !empty(trim($_POST['username']))) {
 	echo "From " . $_POST['username'] . ": " . $_POST['msg'];
 }
 ?>
