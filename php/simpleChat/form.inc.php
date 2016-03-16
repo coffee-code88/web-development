aee625439f0bd9ea467aea7df9c6511a97b3d7e4
coffee-code <coffee.code88@gmail.com>
2016-03-16 19:42:33 +0700

    chat

---
+++ php/simpleChat/form.inc.php
@@ -0,0 +1,5 @@
+<form action="<?php echo $fileName; ?>" method="POST">
+	<input type="text" value="" name="username" />
+	<input type="textarea" value="" name="msg" />
+	<input type="submit" value="Submit" name="submit" />
+</form>
---
+++ php/simpleChat/sender.php
@@ -0,0 +1,9 @@
+<?php
+$fileName="receiver.php";
+
+include 'form.inc.php';
+
+if(isset($_POST['submit']) && !empty($_POST['username'])) {
+	echo "From " . $_POST['username'] . ": " . $_POST['msg'];
+}
+?>
